d932690ddf9e99eb00174ef8400d7c5a68bcef9c
김재훈 <junseak11051@gmail.com>
2022-04-09 16:24:38 +0900

    사용자관리페이지 admin_member.php 기초 완성

--- include/common_function.php
+++ include/common_function.php
@@ -2,22 +2,28 @@
 error_reporting( E_ALL );
 ini_set( "display_errors", 1 );
 include "db.php";
+$func = isset($_GET['func']) ? $_GET['func'] : "";
+
+if($func == "db_delete"){
+    db_delete($func,$_GET['db'],$_GET['idx'],$_SERVER['HTTP_REFERER']);
+};
+
+
 //게시글 페이지 갯수 연산 시작
 function page_count($tablename,$list_length,$list_no_selected,$list_where){ //파라미터는 테이블 명, 페이지당 원하는 게시글 수,선택된 페이지 숫자,조건값where
     include "db.php";
     $sql = "select count(*) from $tablename $list_where";
     $result = mysqli_query($conn,$sql);
     $list_count = mysqli_fetch_array($result);
-    $list_per_page = $list_length; //페이지당 원하는 게시글 수;
-    $list_page_no_selected = $list_no_selected * $list_per_page; 
+    $list_page_no_selected = $list_no_selected * $list_length; 
     
-    $list_page_no = $list_count[0]/20; //게시글 페이지 갯수
+    $list_page_no = $list_count[0]/$list_length; //게시글 페이지 갯수
     if($list_count[0]<$list_length){ //지정한 페이지당 게시글 수보다 적은 경우 식별용 데이터
         $list_less_then_length = "true";
     }else{
         $list_less_then_length = "false";
     }
-    return array($list_page_no_selected,floor($list_page_no),$list_less_then_length);
+    return array($list_page_no_selected,floor($list_page_no),$list_less_then_length); //선택된 페이지 숫자, 페이지 갯수, 페이지 숫자 1이하
 };
 //게시글 페이지 갯수 연산 끝
 
@@ -64,11 +70,12 @@ function inquiry_N_count(){
 // 전체 방문자수 fetch 끝
 
 // 삭제 처리문 모음
-function db_delete($func,$idx){
+
+function db_delete($func,$db,$idx,$prevPage){
     include "db.php";
-    if($func == "post"){
+    if($db == "post"){
         $sql = "delete from post where idx = $idx;";
-    }elseif($func == "member"){
+    }elseif($db == "member"){
         $sql = "delete from member where idx = $idx;";
     }
     
