2fc71b9b06ee286d3c230ba44c5827dde40b3675
jh901 <jh901794@gmail.com>
2022-05-30 02:03:07 +0900

    문제 작업들 완료

--- My_page_write.php
+++ My_page_write.php
@@ -5,16 +5,9 @@ ini_set("display_errors", 1);
 $userID = $_COOKIE["userID"];
 include "include/db.php";
 include "include/common_function.php";
-//$sql = "select * from post where writer_id = '$userID' order by idx";
 
-//5자리에 받아올 거 넣으면 됨
-$sql_user = "select * from member where userID ='$userID'";
-$result_user = mysqli_query($conn, $sql_user);
-
-//$result_write = mysqli_query($conn, $sql);
-
-$category_val = isset($_POST["category_val"])? $_POST["category_val"] : "";
-$sb_val =isset($_POST["sb_val"])? $_POST["sb_val"] : "";
+$category_val = isset($_POST["category_val"]) ? $_POST["category_val"] : "";
+$sb_val = isset($_POST["sb_val"]) ? $_POST["sb_val"] : "";
 
 $query = "select count(*) as count from post where writer_id = '$userID'";
 $data = $conn->query($query)->fetch_array();
@@ -27,7 +20,7 @@ $list_num = 20;
 $page_num = 10;
 
 /* paging : 현재 페이지 */
-$page = isset($_GET["page"])? $_GET["page"] : 1;
+$page = isset($_GET["page"]) ? $_GET["page"] : 1;
 
 /* paging : 전체 페이지 수 = 전체 데이터 / 페이지당 데이터 개수, ceil : 올림값, floor : 내림값, round : 반올림 */
 $total_page = ceil($num / $list_num);
@@ -42,14 +35,14 @@ $now_block = ceil($page / $page_num);
 /* paging : 블럭 당 시작 페이지 번호 = (해당 글의 블럭번호 - 1) * 블럭당 페이지 수 + 1 */
 $s_pageNum = ($now_block - 1) * $page_num + 1;
 // 데이터가 0개인 경우
-if($s_pageNum <= 0){
+if ($s_pageNum <= 0) {
     $s_pageNum = 1;
 };
 
 /* paging : 블럭 당 마지막 페이지 번호 = 현재 블럭 번호 * 블럭 당 페이지 수 */
 $e_pageNum = $now_block * $page_num;
 // 마지막 번호가 전체 페이지 수를 넘지 않도록
-if($e_pageNum > $total_page){
+if ($e_pageNum > $total_page) {
     $e_pageNum = $total_page;
 };
 
@@ -58,14 +51,14 @@ $start = ($page - 1) * $list_num;
 
 /* paging : 쿼리 작성 - limit 몇번부터, 몇개 */
 $sql = "select * from post where writer_id = '$userID'";
-if($category_val != "" && $sb_val != ""){
-    $sql = $sql."where category = '$category_val' and (content like '%$sb_val%' or title like '%$sb_val%') ";
-}else if($category_val != ""){
-    $sql = $sql."where category = '$category_val' ";
-}else{
-    $sql = $sql."where content like '%$sb_val%' or title like '%$sb_val%' ";
+if ($category_val != "" && $sb_val != "") {
+    $sql = $sql . "where category = '$category_val' and (content like '%$sb_val%' or title like '%$sb_val%') ";
+} else if ($category_val != "") {
+    $sql = $sql . "where category = '$category_val' ";
+} else {
+    //$sql = $sql . "where content like '%$sb_val%' or title like '%$sb_val%' ";
 }
-$sql = $sql."order by idx desc limit $start, $list_num ;";
+$sql = $sql . "order by idx desc limit $start, $list_num ;";
 
 /* paging : 쿼리 전송 */
 $result = mysqli_query($conn, $sql);
@@ -104,9 +97,13 @@ $cnt = $start + 1;
                 <td><?php echo $row['recommend_Y']; ?></td>
                 <td>
                     <form method="post" action="user_board_modify_form.php">
-                        <button class="btn_modify" type='submit' value="<?= $row['idx']?>" name='idx'>수정</button>
-                    </form></td>
-                <td><button class="btn_list" onclick="location.href='user_board_delete.php?idx=<?= $row['idx']?>'">삭제</button></td>
+                        <button class="btn_modify" type='submit' value="<?= $row['idx'] ?>" name='idx'>수정</button>
+                    </form>
+                </td>
+                <td>
+                    <button class="btn_list" onclick="location.href='user_board_delete.php?idx=<?= $row['idx'] ?>'">삭제
+                    </button>
+                </td>
             </tr>
             <?php
         };
@@ -116,31 +113,34 @@ $cnt = $start + 1;
         <p>
             <?php
             /* paging : 이전 페이지 */
-            if($page <= 1){
+            if ($page <= 1) {
                 ?>
-                <a href="user_board_list.php?page=1">이전</a>
-            <?php } else{ ?>
-                <a href="user_board_list.php?page=<?php echo ($page-1); ?>">이전</a>
-            <?php };?>
+                <a href="My_page_wrrite.php?page=1">이전</a>
+            <?php } else { ?>
+                <a href="My_page_wrrite.php?page=<?php echo($page - 1); ?>">이전</a>
+            <?php }; ?>
 
             <?php
             /* pager : 페이지 번호 출력 */
-            for($print_page = $s_pageNum; $print_page <= $e_pageNum; $print_page++){
+            for ($print_page = $s_pageNum; $print_page <= $e_pageNum; $print_page++) {
                 ?>
-                <a href="user_board_list.php?page=<?php echo $print_page; ?>"><?php echo $print_page; ?></a>
-            <?php };?>
+                <a href="My_page_wrrite.php?page=<?php echo $print_page; ?>"><?php echo $print_page; ?></a>
+            <?php }; ?>
 
             <?php
             /* paging : 다음 페이지 */
-            if($page >= $total_page){
+            if ($page >= $total_page) {
                 ?>
-                <a href="user_board_list.php?page=<?php echo $total_page; ?>">다음</a>
-            <?php } else{ ?>
-                <a href="user_board_list.php?page=<?php echo ($page+1); ?>">다음</a>
-            <?php };?>
+                <a href="My_page_wrrite.php?page=<?php echo $total_page; ?>">다음</a>
+            <?php } else { ?>
+                <a href="My_page_wrrite.php?page=<?php echo($page + 1); ?>">다음</a>
+            <?php }; ?>
         </p>
     </div>
 </div>
 </body>
 <?php include "include/footer.php"; ?>
 </html>
+<?php
+mysqli_close($conn);
+?>
